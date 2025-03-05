973a9f8b77de2916c02d492b0ef6f5671f4441d0
lonelyexams <furvisiuu6@gmail.com>
2025-03-05 18:59:07 +0800

    Update freeroot_multi_os.sh

--- freeroot_multi_os.sh
+++ freeroot_multi_os.sh
@@ -8,91 +8,14 @@ OS_LIST_JSON='{
       "name": "Debian",
       "version": "11",
       "url": "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.0.0-amd64-netinst.iso",
-      "checksum": "sha256:abc123..."
+      "checksum": "abc123..."
     },
     {
       "id": 2,
       "name": "Ubuntu",
       "version": "22.04",
       "url": "https://releases.ubuntu.com/22.04/ubuntu-22.04-live-server-amd64.iso",
-      "checksum": "sha256:def456..."
-    },
-    {
-      "id": 3,
-      "name": "Arch Linux",
-      "version": "latest",
-      "url": "https://mirror.rackspace.com/archlinux/iso/latest/archlinux-2023.10.14-x86_64.iso",
-      "checksum": "sha256:ghi789..."
-    },
-    {
-      "id": 4,
-      "name": "Fedora",
-      "version": "38",
-      "url": "https://download.fedoraproject.org/pub/fedora/linux/releases/38/Server/x86_64/iso/Fedora-Server-dvd-x86_64-38-1.6.iso",
-      "checksum": "sha256:fedora_checksum_here"
-    },
-    {
-      "id": 5,
-      "name": "CentOS",
-      "version": "9",
-      "url": "https://isoredirect.centos.org/centos/9/isos/x86_64/CentOS-9-latest-x86_64-dvd1.iso",
-      "checksum": "sha256:centos_checksum_here"
-    },
-    {
-      "id": 6,
-      "name": "openSUSE",
-      "version": "Leap 15.4",
-      "url": "https://download.opensuse.org/distribution/leap/15.4/iso/openSUSE-Leap-15.4-DVD-x86_64.iso",
-      "checksum": "sha256:opensuse_checksum_here"
-    },
-    {
-      "id": 7,
-      "name": "Kali Linux",
-      "version": "2023.3",
-      "url": "https://cdimage.kali.org/kali-2023.3/kali-linux-2023.3-installer-amd64.iso",
-      "checksum": "sha256:kali_checksum_here"
-    },
-    {
-      "id": 8,
-      "name": "Alpine Linux",
-      "version": "3.18",
-      "url": "https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-standard-3.18.0-x86_64.iso",
-      "checksum": "sha256:alpine_checksum_here"
-    },
-    {
-      "id": 9,
-      "name": "Gentoo",
-      "version": "latest",
-      "url": "https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/latest-install-amd64-minimal.iso",
-      "checksum": "sha256:gentoo_checksum_here"
-    },
-    {
-      "id": 10,
-      "name": "FreeBSD",
-      "version": "13.2",
-      "url": "https://download.freebsd.org/releases/amd64/amd64/ISO-IMAGES/13.2/FreeBSD-13.2-RELEASE-amd64-disc1.iso",
-      "checksum": "sha256:freebsd_checksum_here"
-    },
-    {
-      "id": 11,
-      "name": "OpenBSD",
-      "version": "7.3",
-      "url": "https://cdn.openbsd.org/pub/OpenBSD/7.3/amd64/install73.iso",
-      "checksum": "sha256:openbsd_checksum_here"
-    },
-    {
-      "id": 12,
-      "name": "NetBSD",
-      "version": "9.3",
-      "url": "https://cdn.netbsd.org/pub/NetBSD/NetBSD-9.3/images/NetBSD-9.3-amd64.iso",
-      "checksum": "sha256:netbsd_checksum_here"
-    },
-    {
-      "id": 13,
-      "name": "Manjaro",
-      "version": "22.1.0",
-      "url": "https://download.manjaro.org/kde/22.1.0/manjaro-kde-22.1.0-230529-linux61.iso",
-      "checksum": "sha256:manjaro_checksum_here"
+      "checksum": "def456..."
     }
   ]
 }'
@@ -123,7 +46,10 @@ download_os() {
   wget "$url" -O "$filename"
 
   echo "Verifying checksum..."
-  if echo "$checksum $filename" | sha256sum --check; then
+  # Remove the "sha256:" prefix from the checksum
+  checksum=$(echo "$checksum" | sed 's/^sha256://')
+
+  if echo "$checksum  $filename" | sha256sum --check; then
     echo "Checksum verified."
   else
     echo "Checksum verification failed!"
